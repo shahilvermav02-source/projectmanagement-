4b22269f66db70d71d887ab07ce20886b5b7fea3
Shahil Verma <shahil.vermav02@gmail.com>
2026-06-01 21:42:20 +0545

    Remove src folder from git tracking

--- src/routes/helathcheck.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express"
-import { healthcheck } from "../controllers/healtcheck.controller.js"
-
-const router = Router()
-router.route("/").get(healthcheck)
-
-export default router
